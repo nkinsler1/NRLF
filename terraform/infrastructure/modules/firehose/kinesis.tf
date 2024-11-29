9f1c2d1c39761c6d2fa6ca2de37189ded13f8030
jackleary <leary.jack@me.com>
2024-11-29 16:35:17 +0000

    NRL-1188 reporting kinesis stream

--- terraform/infrastructure/modules/firehose/kinesis.tf
+++ terraform/infrastructure/modules/firehose/kinesis.tf
@@ -56,3 +56,32 @@ resource "aws_kinesis_firehose_delivery_stream" "firehose" {
     }
   }
 }
+
+resource "aws_kinesis_firehose_delivery_stream" "reporting_stream" {
+  name        = "${var.prefix}--cloudwatch-reporting-delivery-stream"
+  destination = var.destination
+
+  extended_s3_configuration {
+    role_arn   = aws_iam_role.firehose.arn
+    bucket_arn = aws_s3_bucket.reporting.arn
+
+    processing_configuration {
+      enabled = "true"
+
+      processors {
+        type = "CloudWatchLogProcessing"
+
+        parameters {
+          parameter_name  = "DataMessageExtraction"
+          parameter_value = "true"
+        }
+      }
+    }
+
+    cloudwatch_logging_options {
+      enabled         = true
+      log_group_name  = aws_cloudwatch_log_group.firehose.name
+      log_stream_name = aws_cloudwatch_log_stream.firehose.name
+    }
+  }
+}
