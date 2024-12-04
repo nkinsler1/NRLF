f626370b87d3455bcd1480d4eee8d1ab5863bcf2
eesa456 <eesa.mahmood1@nhs.net>
2024-12-04 13:06:37 +0000

    NRL-1051 change tests

--- api/producer/processTransaction/tests/test_process_transaction_bundle.py
+++ api/producer/processTransaction/tests/test_process_transaction_bundle.py
@@ -69,10 +69,7 @@ def test_create_single_nrl_document_reference_with_transaction_happy_path(
         "type": "transaction-response",
         "entry": [
             {
-                "response": {
-                    "status": "201",
-                    "location": "/producer/FHIR/R4/DocumentReference/Y05868-00000000-0000-0000-0000-000000000001",
-                },
+                "response": {"status": "201"},
             },
         ],
     }
@@ -149,7 +146,6 @@ def test_create_single_mhds_document_reference_with_transaction_happy_path(
             {
                 "response": {
                     "status": "201",
-                    "location": "/producer/FHIR/R4/DocumentReference/Y05868-00000000-0000-0000-0000-000000000001",
                 },
             },
         ],
