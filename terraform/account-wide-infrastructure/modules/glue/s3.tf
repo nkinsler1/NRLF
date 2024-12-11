8535b4df22176c51bcdff8f085b959cba08996b1
jackleary <leary.jack@me.com>
2024-12-11 08:50:29 +0000

    NRL-1188 Public access block added

--- terraform/account-wide-infrastructure/modules/glue/s3.tf
+++ terraform/account-wide-infrastructure/modules/glue/s3.tf
@@ -3,15 +3,42 @@ resource "aws_s3_bucket" "source-data-bucket" {
   bucket = "source-data-bucket"
 }
 
+resource "aws_s3_bucket_public_access_block" "source-data-bucket-public-access-block" {
+  bucket = aws_s3_bucket.source-data-bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
 # S3 Bucket for Processed Data
 resource "aws_s3_bucket" "target-data-bucket" {
   bucket = "target-data-bucket"
 }
 
+resource "aws_s3_bucket_public_access_block" "target-data-bucket-public-access-block" {
+  bucket = aws_s3_bucket.target-data-bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
 
 # S3 Bucket for Code
 resource "aws_s3_bucket" "code-bucket" {
   bucket = "code-bucket"
+}
+
+resource "aws_s3_bucket_public_access_block" "code-bucket-public-access-block" {
+  bucket = aws_s3_bucket.code-bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_object" "code-data-object" {
