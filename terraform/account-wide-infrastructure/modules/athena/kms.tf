601209106596e7e7265eb3cd5b9df974281305ff
jackleary <leary.jack@me.com>
2024-12-13 14:45:51 +0000

    NRL-1188 Fix var name

--- terraform/account-wide-infrastructure/modules/athena/kms.tf
+++ terraform/account-wide-infrastructure/modules/athena/kms.tf
@@ -2,6 +2,6 @@ resource "aws_kms_key" "athena" {
 }
 
 resource "aws_kms_alias" "athena" {
-  name          = "alias/${var.prefix}-athena"
+  name          = "alias/${var.name_prefix}-athena"
   target_key_id = aws_kms_key.athena.key_id
 }
