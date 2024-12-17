043ccd3ed399f72661579427755d7e031ad60ce0
jackleary <leary.jack@me.com>
2024-12-17 09:10:12 +0000

    NRL-1188 Pass s3 bucket arn as var

--- terraform/infrastructure/firehose.tf
+++ terraform/infrastructure/firehose.tf
@@ -1,12 +1,12 @@
 module "firehose__processor" {
-  source             = "./modules/firehose"
-  assume_account     = local.aws_account_id
-  prefix             = local.prefix
-  region             = local.region
-  environment        = local.environment
-  cloudwatch_kms_arn = module.kms__cloudwatch.kms_arn
-  splunk_environment = local.splunk_environment
-  splunk_index       = local.splunk_index
-  destination        = "splunk"
-  reporting_bucket   = data.aws_s3_bucket.source-data-bucket
+  source               = "./modules/firehose"
+  assume_account       = local.aws_account_id
+  prefix               = local.prefix
+  region               = local.region
+  environment          = local.environment
+  cloudwatch_kms_arn   = module.kms__cloudwatch.kms_arn
+  splunk_environment   = local.splunk_environment
+  splunk_index         = local.splunk_index
+  destination          = "splunk"
+  reporting_bucket_arn = data.aws_s3_bucket.source-data-bucket.arn
 }
