fa711eb12563ba70bfb0d0b25977164215bcb224
jackleary <leary.jack@me.com>
2024-12-17 08:20:35 +0000

    NRL-1188 Pass s3 bucket arn as var

--- terraform/infrastructure/firehose.tf
+++ terraform/infrastructure/firehose.tf
@@ -8,4 +8,5 @@ module "firehose__processor" {
   splunk_environment = local.splunk_environment
   splunk_index       = local.splunk_index
   destination        = "splunk"
+  reporting_bucket   = data.aws_s3_bucket.source-data-bucket.arn
 }
