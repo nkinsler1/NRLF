e331b8f39b1a5d92892eb37cf88158a324d6f760
jackleary <leary.jack@me.com>
2024-12-18 13:11:32 +0000

    NRL-1188 Update env condition

--- terraform/infrastructure/locals.tf
+++ terraform/infrastructure/locals.tf
@@ -22,7 +22,7 @@ locals {
   dynamodb_timeout_seconds = "3"
 
   is_sandbox_env = length(regexall("-sandbox-", local.stack_name)) > 0
-  is_dev_env     = length(regexall("dev", local.stack_name)) > 0
+  is_dev_env     = var.account_name == "dev"
 
   environment   = local.is_sandbox_env ? "${var.account_name}-sandbox" : var.account_name
   shared_prefix = "${local.project}--${local.environment}"
