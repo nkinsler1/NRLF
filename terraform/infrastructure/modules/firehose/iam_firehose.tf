160fd5bda14a3be2fe839f1adbd9e1ce6b7d691a
jackleary <leary.jack@me.com>
2024-12-18 13:01:28 +0000

    NRL-1188 use compact to ignore nulls

--- terraform/infrastructure/modules/firehose/iam_firehose.tf
+++ terraform/infrastructure/modules/firehose/iam_firehose.tf
@@ -31,7 +31,6 @@ data "aws_iam_policy_document" "firehose" {
       aws_s3_bucket.firehose.arn,
       "${aws_s3_bucket.firehose.arn}/*",
       var.reporting_bucket_arn,
-      "${var.reporting_bucket_arn}/*",
     ])
     effect = "Allow"
   }
