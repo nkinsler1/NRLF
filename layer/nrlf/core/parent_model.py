a4b10ac120c516d2a315e6bf547f862fc4e270b9
eesa456 <eesa.mahmood1@nhs.net>
2025-02-07 14:05:08 +0000

    NRL-786 add extension to parent model

--- layer/nrlf/core/parent_model.py
+++ layer/nrlf/core/parent_model.py
@@ -1,5 +1,25 @@
-from pydantic import BaseModel, ConfigDict
+from typing import Annotated, List, Optional
+
+from consumer.fhir.r4.model import CodeableConcept
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class Extension(BaseModel):
+    model_config = ConfigDict(regex_engine="python-re", extra="forbid")
+    valueCodeableConcept: Annotated[
+        Optional[CodeableConcept],
+        Field(
+            description="A name which details the functional use for this link &ndash; see [http://www.iana.org/assignments/link&ndash;relations/link&ndash;relations.xhtml#link&ndash;relations&ndash;1](http://www.iana.org/assignments/link&ndash;relations/link&ndash;relations.xhtml#link&ndash;relations&ndash;1)."
+        ),
+    ] = None
+    url: Annotated[
+        Optional[str],
+        Field(description="The reference details for the link.", pattern="\\S*"),
+    ] = None
 
 
 class Parent(BaseModel):
     model_config = ConfigDict(regex_engine="python-re", extra="forbid")
+    extension: Annotated[
+        Optional[List[Extension]], Field(description="A list of relevant extensions")
+    ]
