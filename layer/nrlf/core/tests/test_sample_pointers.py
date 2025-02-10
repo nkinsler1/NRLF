9d37f6bf3117a271d0870c2a76e236b87938c4f9
Matt Dean <matt.dean3@nhs.net>
2025-02-10 16:52:35 +0000

    [NRL-1279] Fix up unit test for sample pointer. Add redacted live sample pointers

--- layer/nrlf/core/tests/test_sample_pointers.py
+++ layer/nrlf/core/tests/test_sample_pointers.py
@@ -7,29 +7,30 @@ from nrlf.core.validators import DocumentReferenceValidator
 from nrlf.producer.fhir.r4.model import DocumentReference as ProducerDocumentReference
 
 
-@pytest.fixture
-def sample_pointer_data() -> str:
-    json_files = [f for f in os.listdir("./tests/data/samples") if f.endswith(".json")]
-
-    for file in json_files:
-        with open(f"./tests/data/samples/{file}", "r") as f:
-            pointer_data = f.read()
-            yield pointer_data
+def sample_pointer_files() -> list[str]:
+    return [f for f in os.listdir("./tests/data/samples") if f.endswith(".json")]
 
 
-def test_sample_pointer_as_consumer(sample_pointer_data: str):
+def load_sample_pointer_data(pointer_file: str) -> str:
+    with open(f"./tests/data/samples/{pointer_file}", "r") as f:
+        return f.read()
+
+
+@pytest.mark.parametrize("sample_pointer_file", sample_pointer_files())
+def test_sample_pointer_as_consumer(sample_pointer_file: str):
+    sample_pointer_data = load_sample_pointer_data(sample_pointer_file)
+
     docref = ConsumerDocumentReference.model_validate_json(sample_pointer_data)
-
-    validator = DocumentReferenceValidator()
-    result = validator.validate(data=docref)
+    result = DocumentReferenceValidator().validate(data=docref)
 
     assert result.is_valid
 
 
-def test_sample_pointer_as_producer(sample_pointer_data: str):
-    docref = ProducerDocumentReference.model_validate_json(sample_pointer_data)
+@pytest.mark.parametrize("sample_pointer_file", sample_pointer_files())
+def test_sample_pointer_as_producer(sample_pointer_file: str):
+    sample_pointer_data = load_sample_pointer_data(sample_pointer_file)
 
-    validator = DocumentReferenceValidator()
-    result = validator.validate(data=docref)
+    docref = ProducerDocumentReference.model_validate_json(sample_pointer_data)
+    result = DocumentReferenceValidator().validate(data=docref)
 
     assert result.is_valid
