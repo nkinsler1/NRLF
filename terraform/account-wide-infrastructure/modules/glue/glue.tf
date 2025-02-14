52bb75d228265f094ce0a8ac337bd9f2079c6fff
jackleary <leary.jack@me.com>
2025-02-14 14:45:05 +0000

    NRL-1288 framework for multiple schema submission

--- terraform/account-wide-infrastructure/modules/glue/glue.tf
+++ terraform/account-wide-infrastructure/modules/glue/glue.tf
@@ -49,8 +49,8 @@ resource "aws_glue_job" "glue_job" {
     "--enable-auto-scaling"             = "true"
     "--enable-continous-cloudwatch-log" = "true"
     "--datalake-formats"                = "delta"
-    "--source_path"                     = "s3://${aws_s3_bucket.source-data-bucket.id}/"     # Specify the source S3 path
-    "--target_path"                     = "s3://${aws_s3_bucket.target-data-bucket.id}/logs" # Specify the destination S3 path
+    "--source_path"                     = "s3://${aws_s3_bucket.source-data-bucket.id}/" # Specify the source S3 path
+    "--target_path"                     = "s3://${aws_s3_bucket.target-data-bucket.id}/" # Specify the destination S3 path
     "--job_name"                        = "${var.name_prefix}-glue-job"
     "--partition_cols"                  = "date"
     "--enable-continuous-log-filter"    = "true"
--- terraform/account-wide-infrastructure/modules/glue/src/main.py
+++ terraform/account-wide-infrastructure/modules/glue/src/main.py
@@ -3,7 +3,8 @@ import sys
 from awsglue.utils import getResolvedOptions
 from pipeline import LogPipeline
 from pyspark.context import SparkContext
-from transformations import dtype_conversion, flatten_df, logSchema
+from schemas import schemas
+from transformations import dtype_conversion, flatten_df
 
 # Get arguments from AWS Glue job
 args = getResolvedOptions(
@@ -20,7 +21,7 @@ etl_job = LogPipeline(
     spark_context=sc,
     source_path=args["source_path"],
     target_path=args["target_path"],
-    schema=logSchema,
+    schema=schemas,
     job_name=args["job_name"],
     partition_cols=partition_cols,
     transformations=[flatten_df, dtype_conversion],
