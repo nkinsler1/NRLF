98e2f9687506c9f60f38c87ad76dc65a5fb22dca
jackleary <leary.jack@me.com>
2025-02-28 17:08:37 +0000

    NRL-1320 sonar changes

--- terraform/account-wide-infrastructure/modules/glue/src/transformations.py
+++ terraform/account-wide-infrastructure/modules/glue/src/transformations.py
@@ -44,22 +44,22 @@ def flatten_df(df):
     while len(complex_fields) != 0:
         col_name = list(complex_fields.keys())[0]
 
-        if type(complex_fields[col_name]) == StructType:
+        if isinstance(complex_fields[col_name], StructType):
             expanded = [
                 col(col_name + "." + k).alias(col_name + "_" + k)
                 for k in [n.name for n in complex_fields[col_name]]
             ]
             df = df.select("*", *expanded).drop(col_name)
 
-        elif type(complex_fields[col_name]) == ArrayType:
+        elif isinstance(complex_fields[col_name], ArrayType):
             df = df.withColumn(col_name, explode_outer(col_name))
 
         complex_fields = dict(
             [
                 (field.name, field.dataType)
                 for field in df.schema.fields
-                if type(field.dataType) == ArrayType
-                or type(field.dataType) == StructType
+                if isinstance(field.dataType, ArrayType)
+                or isinstance(field.dataType, ArrayType)
             ]
         )
     return df
