96a5e287c8cffb067cacb73f75ed570d24f8fb28
jackleary <leary.jack@me.com>
2025-03-03 09:36:25 +0000

    NRL-1320 Exit if df is null

--- terraform/account-wide-infrastructure/modules/glue/src/pipeline.py
+++ terraform/account-wide-infrastructure/modules/glue/src/pipeline.py
@@ -90,12 +90,11 @@ class LogPipeline:
         """Load transformed data into Parquet format"""
         self.logger.info(f"Loading data into {self.target_path} as Parquet")
         for name, dataframe in data.items():
-            if not dataframe:
-                continue
-            name = name.replace("--", "_")
-            dataframe.write.mode("append").partitionBy(*self.partition_cols).parquet(
-                f"{self.target_path}{name}"
-            )
+            if dataframe.na.drop().count() > 0:
+                name = name.replace("--", "_")
+                dataframe.write.mode("append").partitionBy(
+                    *self.partition_cols
+                ).parquet(f"{self.target_path}{name}")
 
     def trigger_crawler(self):
         self.glue.start_crawler(Name=f"{self.name_prefix}-log-crawler")
