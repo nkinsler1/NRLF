8483dd25afd9550d31dcaf45c24d27299888e8e5
jackleary <leary.jack@me.com>
2025-05-02 09:32:06 +0100

    NRL-1385 base ec2 set up

---
+++ terraform/account-wide-infrastructure/modules/ec2/ec2.tf
@@ -0,0 +1,97 @@
+module "autoscaling" {
+  source  = "registry.terraform.io/terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+
+  name = "PowerBI-On-Premise-Gateway"
+
+  min_size         = 0
+  max_size         = 1
+  desired_capacity = 1
+
+  # Autoscaling Schedule
+  schedules = {
+    morning_start = {
+      min_size         = -1
+      max_size         = -1
+      desired_capacity = 1
+      recurrence       = "50 5 * * 0-6"
+      time_zone        = "Europe/Paris"
+    }
+
+    morning_stop = {
+      min_size         = -1
+      max_size         = -1
+      desired_capacity = 0
+      recurrence       = "30 6 * * 0-6"
+      time_zone        = "Europe/Paris"
+    }
+
+    noon_start = {
+      min_size         = -1
+      max_size         = -1
+      desired_capacity = 1
+      recurrence       = "50 11 * * 0-6"
+      time_zone        = "Europe/Paris"
+    }
+
+    noon_stop = {
+      min_size         = -1
+      max_size         = -1
+      desired_capacity = 0
+      recurrence       = "30 12 * * 0-6"
+      time_zone        = "Europe/Paris"
+    }
+
+    evening_start = {
+      min_size         = -1
+      max_size         = -1
+      desired_capacity = 1
+      recurrence       = "50 17 * * 0-6"
+      time_zone        = "Europe/Paris"
+    }
+
+    evening_stop = {
+      min_size         = -1
+      max_size         = -1
+      desired_capacity = 0
+      recurrence       = "30 18 * * 0-6"
+      time_zone        = "Europe/Paris"
+    }
+  }
+
+  wait_for_capacity_timeout = 0
+  health_check_type         = "EC2"
+  health_check_grace_period = 300
+  enable_monitoring         = false
+
+  #image_id                = data.aws_ami.windows.id  // Phase 2 : we let the Auto Scaling Group use the AMI we've juste created.
+  image_id                = data.aws_ami.final.id
+  launch_template_version = "$Latest"
+  instance_type           = "m5a.large"
+
+  instance_market_options = {
+    market_type = "spot"
+  }
+
+  # Refresh instances when redeploying
+  instance_refresh = {
+    strategy = "Rolling"
+    triggers = ["tag"]
+  }
+
+  # Assign a role to the instance
+  create_iam_instance_profile = true
+  iam_role_name               = "powerbi-gateway-role"
+  iam_role_description        = "Allow the Power BI Gateway to be managed by"
+  iam_role_policies = {
+    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
+  key_name = aws_key_pair.ec2_key_pair.key_name
+
+  vpc_zone_identifier = data.aws_subnets.subnets.ids
+
+  security_groups = [module.security-group-outbound.security_group_id]
+
+  user_data              = filebase64("./userdata.txt")
+  update_default_version = true
+}
