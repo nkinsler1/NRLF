f8dc52028931e32e6e912b79240f4c989ef6f0b0
jackleary <leary.jack@me.com>
2025-05-11 16:26:49 +0100

    NRL-1385 security fix

--- terraform/account-wide-infrastructure/modules/vpc/vpc.tf
+++ terraform/account-wide-infrastructure/modules/vpc/vpc.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "sg" {
     from_port        = 3389
     to_port          = 3389
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = [var.vpc_public_subnets_cidr_block]
     ipv6_cidr_blocks = ["::/0"]
   }
 
