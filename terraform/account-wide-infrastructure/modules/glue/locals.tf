e69b469e74027f6777ecd76af5cd5acda9303da9
jackleary <leary.jack@me.com>
2025-05-19 19:29:26 +0100

    NRL-1486 Expire logs after 1 day. Up firehose filesize to further lower number of files

--- terraform/account-wide-infrastructure/modules/glue/locals.tf
+++ terraform/account-wide-infrastructure/modules/glue/locals.tf
@@ -1,18 +1,7 @@
 locals {
   s3 = {
-    transition_storage = {
-      infrequent_access = {
-        storage_class = "STANDARD_IA"
-        days          = 2
-      }
-      glacier = {
-        storage_class = "GLACIER"
-        days          = 7
-      }
-    }
-
     expiration = {
-      days = 1095
+      days = 1
     }
   }
 }
