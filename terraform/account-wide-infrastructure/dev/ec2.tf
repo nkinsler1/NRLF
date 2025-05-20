072ad8b7662595dd085dbcee88f05e0d5729b1ba
Matt Dean <matt.dean3@nhs.net>
2025-05-20 08:45:40 +0100

    [NRL-1386] Switch PowerBI GW to private subnet. Add SSM access policy

--- terraform/account-wide-infrastructure/dev/ec2.tf
+++ terraform/account-wide-infrastructure/dev/ec2.tf
@@ -1,12 +1,12 @@
 module "vpc" {
-  source                        = "../modules/vpc"
-  vpc_cidr_block                = var.vpc_cidr_block
-  enable_dns_hostnames          = var.enable_dns_hostnames
-  vpc_public_subnets_cidr_block = var.vpc_public_subnets_cidr_block
-  aws_azs                       = var.aws_azs
-  name_prefix                   = "nhsd-nrlf--dev"
+  source                         = "../modules/vpc"
+  vpc_cidr_block                 = var.vpc_cidr_block
+  enable_dns_hostnames           = var.enable_dns_hostnames
+  vpc_public_subnets_cidr_block  = var.vpc_public_subnets_cidr_block
+  vpc_private_subnets_cidr_block = var.vpc_private_subnets_cidr_block
+  aws_azs                        = var.aws_azs
+  name_prefix                    = "nhsd-nrlf--dev"
 }
-
 
 module "ec2" {
   source             = "../modules/ec2"
@@ -20,4 +20,17 @@ module "ec2" {
 
   subnet_id       = module.vpc.subnet_id
   security_groups = module.vpc.security_group
+}
+
+module "powerbi_gw_instance" {
+  source             = "../modules/ec2"
+  instance_type      = var.instance_type
+  name_prefix        = "nhsd-nrlf--dev-powerbi-gw"
+  target_bucket_arn  = module.dev-glue.target_bucket_arn
+  glue_kms_key_arn   = module.dev-glue.aws_kms_key_arn
+  athena_kms_key_arn = module.dev-athena.kms_key_arn
+  athena_bucket_arn  = module.dev-athena.bucket_arn
+
+  subnet_id       = module.vpc.private_subnet_id
+  security_groups = [module.vpc.powerbi_gw_security_group_id]
 }
--- terraform/account-wide-infrastructure/modules/vpc/outputs.tf
+++ terraform/account-wide-infrastructure/modules/vpc/outputs.tf
@@ -2,6 +2,14 @@ output "subnet_id" {
   value = aws_subnet.public_subnet.id
 }
 
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
+}
+
 output "security_group" {
   value = [aws_security_group.sg.id]
+}
+
+output "powerbi_gw_security_group_id" {
+  value = aws_security_group.powerbi_gw_sg.id
 }
